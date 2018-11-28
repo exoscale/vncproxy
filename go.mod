98c60a5f408a235e2043e68d59ad8f9ada9c23bb
Marc Falzon <marc@exoscale.com>
2018-11-28 15:27:19 +0100

    Add extended logger based on inconshreveable/log15

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/exoscale/vncproxy
 
-require golang.org/x/net v0.0.0-20181114220301-adae6a3d119a
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	golang.org/x/net v0.0.0-20181129055619-fae4c4e3ad76
+	golang.org/x/sys v0.0.0-20181128092732-4ed8d59d0b35 // indirect
+	gopkg.in/inconshreveable/log15.v2 v2.0.0-20180818164646-67afb5ed74ec
+)
--- logger/logger.go
+++ logger/logger.go
@@ -1,19 +1,15 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
 
-var simpleLogger = SimpleLogger{LogLevelInfo}
+	"gopkg.in/inconshreveable/log15.v2"
+)
 
-func SetLogLevel(logLevel string) {
-	level := GetLogLevel(logLevel)
-	fmt.Println("Log level set to: ", logLevel)
-	simpleLogger = SimpleLogger{level}
-}
+var defaultLogger Logger = &SimpleLogger{LogLevelInfo}
 
 func GetLogLevel(logLevel string) LogLevel {
 	switch logLevel {
-	case "trace":
-		return LogLevelTrace
 	case "debug":
 		return LogLevelDebug
 	case "info":
@@ -22,13 +18,12 @@ func GetLogLevel(logLevel string) LogLevel {
 		return LogLevelWarn
 	case "error":
 		return LogLevelError
-	case "fatal":
-		return LogLevelFatal
 	}
 	return LogLevelInfo
 }
 
 type Logger interface {
+	SetLogLevel(string)
 	Debug(v ...interface{})
 	Debugf(format string, v ...interface{})
 	Info(v ...interface{})
@@ -37,38 +32,24 @@ type Logger interface {
 	Warnf(format string, v ...interface{})
 	Error(v ...interface{})
 	Errorf(format string, v ...interface{})
-	Fatal(v ...interface{})
-	Fatalf(format string, v ...interface{})
 }
+
 type LogLevel int
 
 const (
-	LogLevelTrace LogLevel = iota
-	LogLevelDebug
+	LogLevelDebug LogLevel = iota
 	LogLevelInfo
 	LogLevelWarn
 	LogLevelError
-	LogLevelFatal
 )
 
 type SimpleLogger struct {
 	level LogLevel
 }
 
-func (sl *SimpleLogger) Trace(v ...interface{}) {
-	if sl.level <= LogLevelTrace {
-		arr := []interface{}{"[Trace]"}
-		for _, item := range v {
-			arr = append(arr, item)
-		}
-
-		fmt.Println(arr...)
-	}
-}
-func (sl *SimpleLogger) Tracef(format string, v ...interface{}) {
-	if sl.level <= LogLevelTrace {
-		fmt.Printf("[Trace] "+format+"\n", v...)
-	}
+func (sl *SimpleLogger) SetLogLevel(logLevel string) {
+	level := GetLogLevel(logLevel)
+	sl.level = level
 }
 
 func (sl *SimpleLogger) Debug(v ...interface{}) {
@@ -128,60 +109,80 @@ func (sl *SimpleLogger) Errorf(format string, v ...interface{}) {
 		fmt.Printf("[Error] "+format+"\n", v...)
 	}
 }
-func (sl *SimpleLogger) Fatal(v ...interface{}) {
-	if sl.level <= LogLevelFatal {
-		arr := []interface{}{"[Fatal]"}
-		for _, item := range v {
-			arr = append(arr, item)
-		}
-		fmt.Println(arr...)
 
-	}
+type ExtendedLogger struct {
+	logger log15.Logger
 }
-func (sl *SimpleLogger) Fatalf(format string, v ...interface{}) {
-	if sl.level <= LogLevelFatal {
-		fmt.Printf("[Fatal] "+format+"\n", v)
+
+func NewExtendedLogger(l log15.Logger) *ExtendedLogger {
+	if l == nil {
+		l = log15.Root()
 	}
+
+	return &ExtendedLogger{logger: l}
+}
+
+func (el *ExtendedLogger) SetLogLevel(logLevel string) {
+	level, _ := log15.LvlFromString(logLevel) // Falls back to level 'debug' in case of error
+	el.logger.SetHandler(log15.LvlFilterHandler(level, log15.StdoutHandler))
+}
+func (el *ExtendedLogger) Debug(v ...interface{}) {
+	el.logger.Debug(fmt.Sprint(v...))
+}
+func (el *ExtendedLogger) Debugf(format string, v ...interface{}) {
+	el.logger.Debug(fmt.Sprintf(format, v...))
+}
+func (el *ExtendedLogger) Info(v ...interface{}) {
+	el.logger.Info(fmt.Sprint(v...))
+}
+func (el *ExtendedLogger) Infof(format string, v ...interface{}) {
+	el.logger.Info(fmt.Sprintf(format, v...))
+}
+func (el *ExtendedLogger) Warn(v ...interface{}) {
+	el.logger.Warn(fmt.Sprint(v...))
+}
+func (el *ExtendedLogger) Warnf(format string, v ...interface{}) {
+	el.logger.Warn(fmt.Sprintf(format, v...))
+}
+func (el *ExtendedLogger) Error(v ...interface{}) {
+	el.logger.Error(fmt.Sprint(v...))
+}
+func (el *ExtendedLogger) Errorf(format string, v ...interface{}) {
+	el.logger.Error(fmt.Sprintf(format, v...))
+}
+
+func SetLogger(i interface{}) {
+	defaultLogger = i.(Logger)
+}
+
+func SetLogLevel(logLevel string) {
+	defaultLogger.SetLogLevel(logLevel)
 }
 
 func Debug(v ...interface{}) {
-	simpleLogger.Debug(v...)
+	defaultLogger.Debug(v...)
 }
 func Debugf(format string, v ...interface{}) {
-	simpleLogger.Debugf(format, v...)
-}
-
-func Trace(v ...interface{}) {
-	simpleLogger.Trace(v...)
-}
-func Tracef(format string, v ...interface{}) {
-	simpleLogger.Tracef(format, v...)
+	defaultLogger.Debugf(format, v...)
 }
 
 func Info(v ...interface{}) {
-	simpleLogger.Info(v...)
+	defaultLogger.Info(v...)
 }
 func Infof(format string, v ...interface{}) {
-	simpleLogger.Infof(format, v...)
+	defaultLogger.Infof(format, v...)
 }
 
 func Warn(v ...interface{}) {
-	simpleLogger.Warn(v...)
+	defaultLogger.Warn(v...)
 }
 func Warnf(format string, v ...interface{}) {
-	simpleLogger.Warnf(format, v...)
+	defaultLogger.Warnf(format, v...)
 }
 
 func Error(v ...interface{}) {
-	simpleLogger.Error(v...)
+	defaultLogger.Error(v...)
 }
 func Errorf(format string, v ...interface{}) {
-	simpleLogger.Errorf(format, v...)
-}
-
-func Fatal(v ...interface{}) {
-	simpleLogger.Fatal(v...)
-}
-func Fatalf(format string, v ...interface{}) {
-	simpleLogger.Fatalf(format, v...)
+	defaultLogger.Errorf(format, v...)
 }
